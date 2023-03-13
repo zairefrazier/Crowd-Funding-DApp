ad66cac9093baac06b8d9329bb423aabfa9fcf19
Zaire <60757805+zairefrazier@users.noreply.github.com>
2023-03-13 18:27:29 -0400

    Layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,8 +2,15 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 const App = () => {
   return (
-    <div className="relative sm: -8 p-4 bg-[#13131a] min-h-screen flex flex-row">
-      App
+    <div className="relative sm:-8 p-4 bg-black min-h-screen flex flex-row">
+      <div className="sm:flew hidden mr-10 relative">
+        Sidebar
+
+      </div>
+      <p className="font-xl font-bold">Test</p>
+      <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5">
+        Navbar
+      </div>
     </div>
   )
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,7 @@ import App from "./App";
 import { ReactDOM } from "react-dom/client";
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 import "./styles/globals.css";
+import "./styles/index.css";
 import { BrowserRouter as Router} from "react-router-dom";
 
 // This is the chainId your dApp will work on.
@@ -14,7 +15,9 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <ThirdwebProvider desiredChainId={activeChainId}>
+      <Router>
       <App />
+      </Router>
     </ThirdwebProvider>
   </React.StrictMode>
 );
