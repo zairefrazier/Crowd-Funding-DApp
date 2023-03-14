e7ffd418b9b27fbcf50ad0e8d416a42140090efe
Zaire <60757805+zairefrazier@users.noreply.github.com>
2023-03-14 14:22:44 -0400

    Working on nav

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
+import { useState }  from 'react'
+import { Link, useNavigate} from 'react-router-dom'
+
+
+
+
 
 const Sidebar = () => {
+
+  const navigate = useNavigate();
+
   return (
     <div>Sidebar</div>
   )
