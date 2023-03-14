110978e79a610bdc983db019aef17b770a4f6f0b
Zaire <60757805+zairefrazier@users.noreply.github.com>
2023-03-14 09:39:16 -0400

    Created Components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home"
+import { CampaignDetails, CreateCampaign, Home, Profile }from "./pages";
 
 
 
---
+++ client/src/components/Navbard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Navbard = () => {
+  return (
+    <div>Navbard</div>
+  )
+}
+
+export default Navbard
