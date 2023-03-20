6834eb8d53e7e14a7c6b4cda12b7bd197f35316a
Zaire <60757805+zairefrazier@users.noreply.github.com>
2023-03-20 11:20:29 -0400

    bug fixes

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -4,15 +4,11 @@ import { Link, useNavigate} from 'react-router-dom'
 
 
 
-
-
-
 const Sidebar = () => {
 
   const navigate = useNavigate();
 
   const [isActive, setIsActive] = useState('dashboard')
-
   return (
     <div>Sidebar</div>
   )
