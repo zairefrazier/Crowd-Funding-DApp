28e297a6fb075f5e119fde16e3d23315a93377e7
Zaire <60757805+zairefrazier@users.noreply.github.com>
2023-04-10 13:07:04 -0400

    sidebar

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,15 +1,31 @@
-import React from 'react'
-import { useState }  from 'react'
+import React ,{ useState }  from 'react'
 import { Link, useNavigate} from 'react-router-dom'
 
+import logo from '../assets/logo.svg';
+import sun from '../assets/sun.svg';
+
+
+
+const Icon = ({ styles, name, imgurl, isActive,disabled, handleClick }) => {
+
+  <div>
+
+  </div>
+
+}
 
 const Sidebar = () => {
-
   const navigate = useNavigate();
-
   const [isActive, setIsActive] = useState('dashboard')
+
+
+
   return (
-    <div>Sidebar</div>
+    <div className='flex justify-between items-center flex-col sticky top-5 h-[93vh]'>
+      <Link to="/">
+        <Icon styles = 'w-[52px] h-[52px] bg-[#2c2f32]' imgurl={logo} />
+      </Link>
+    </div>
   )
 }
 
