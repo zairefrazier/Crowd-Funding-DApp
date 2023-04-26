d43f2dcdca3daa08f4c40242b5edf48a633733b3
Zaire <60757805+zairefrazier@users.noreply.github.com>
2023-04-26 17:36:15 -0400

    SideBar Fixed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,14 +7,14 @@ import { SideBar, Navbar } from "./components";
 const App = () => {
   return (
     <div className="relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row">
-      <div className="sm:flew hidden mr-10 relative">
+      <div className="sm:flex hidden mr-10 relative">
         <SideBar />
+      
 
       </div>
-      <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5 text-white">
+      <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5">
         <Navbar />
         <Routes>
-
           <Route path="/" element={ <Home />} />
 
         </Routes>
